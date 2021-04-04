4b21f7939823b6f03a5dffc596f31f06688f8db0
ArtemBrylov <tematibrl@gmail.com>
2021-04-04 20:25:25 +0300

    Fist implementation for unpacking string

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,33 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var sb strings.Builder
+
+	for i, val := range s {
+		if unicode.IsDigit(rune(val)) {
+			if i == 0 || (i >= 1 && unicode.IsDigit(rune(s[i-1]))) {
+				return "", ErrInvalidString
+			}
+		} else {
+			if i+1 == len(s) || !unicode.IsDigit(rune(s[i+1])) {
+				sb.WriteString(string(val))
+			} else if unicode.IsDigit(rune(s[i+1])) {
+				intValue, err := strconv.Atoi(string(s[i+1]))
+				if err != nil {
+					fmt.Println("Error during cast Atoi: " + err.Error())
+				}
+				sb.WriteString(strings.Repeat(string(val), intValue))
+			}
+		}
+	}
+	return sb.String(), nil
 }
