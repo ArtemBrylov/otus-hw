48c3ac33c136899898b8b550010b0b14f9507341
ArtemBrylov <tematibrl@gmail.com>
2021-04-04 21:08:25 +0300

    Fix 'if' complexity

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -14,20 +14,21 @@ func Unpack(s string) (string, error) {
 	var sb strings.Builder
 
 	for i, val := range s {
-		if unicode.IsDigit(rune(val)) {
-			if i == 0 || (i >= 1 && unicode.IsDigit(rune(s[i-1]))) {
-				return "", ErrInvalidString
+		switch {
+		case unicode.IsDigit(val) && i == 0:
+			return "", ErrInvalidString
+		case unicode.IsDigit(val) && i >= 1 && unicode.IsDigit(rune(s[i-1])):
+			return "", ErrInvalidString
+		case unicode.IsLetter(val) && i+1 == len(s):
+			sb.WriteString(string(val))
+		case unicode.IsLetter(val) && !unicode.IsDigit(rune(s[i+1])):
+			sb.WriteString(string(val))
+		case unicode.IsLetter(val) && unicode.IsDigit(rune(s[i+1])):
+			intValue, err := strconv.Atoi(string(s[i+1]))
+			if err != nil {
+				fmt.Println("Error during cast Atoi: " + err.Error())
 			}
-		} else {
-			if i+1 == len(s) || !unicode.IsDigit(rune(s[i+1])) {
-				sb.WriteString(string(val))
-			} else if unicode.IsDigit(rune(s[i+1])) {
-				intValue, err := strconv.Atoi(string(s[i+1]))
-				if err != nil {
-					fmt.Println("Error during cast Atoi: " + err.Error())
-				}
-				sb.WriteString(strings.Repeat(string(val), intValue))
-			}
+			sb.WriteString(strings.Repeat(string(val), intValue))
 		}
 	}
 	return sb.String(), nil
