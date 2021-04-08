1b95125f653b70499c14b753a5f63fbece24af63
tirrex <tematibrl@gmail.com>
2021-04-09 00:43:04 +0300

    HW03. First try of running basic methods for the task

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,42 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+type ChartItem struct {
+	Word   string
+	Amount int
+}
+
+func Top10(text string) []string {
+	// r := fmt.Sprint(len(text))
+	// fmt.Println("Length=" + r)
+
+	textSplitted := strings.Split(text, " ")
+	fmt.Println("==================")
+	fmt.Println(textSplitted)
+	fmt.Println("==================")
+
+	words := []ChartItem{}
+
+	for i, val := range textSplitted {
+		fmt.Println(i)
+		fmt.Println(val)
+
+		// Count how many times current word is presented into text
+
+		words = append(words, ChartItem{val, i})
+	}
+
+	sort.Slice(words, func(i, j int) bool { return words[i].Amount > words[j].Amount })
+
+	for i, val := range words {
+		fmt.Println(i)
+		fmt.Println(val)
+	}
+
 	return nil
 }
