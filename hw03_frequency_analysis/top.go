d5f56c503bcb90f498d2c4e23ae73a26c228154d
tirrex <tematibrl@gmail.com>
2021-04-17 23:34:10 +0300

    Task without asterisk is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -12,32 +12,21 @@ type ChartItem struct {
 	Amount int
 }
 
-// var splitter *regexp.Regexp = regexp.MustCompile(` *, *`)
 var splitterSpaces *regexp.Regexp = regexp.MustCompile("\\s+")
 
 func Top10(text string) []string {
 
-	textInOneRow := strings.ReplaceAll(text, "\n", "")
+	textInOneRow := strings.ReplaceAll(text, "\n", " ")
 	textWithoutTabs := strings.ReplaceAll(textInOneRow, "\t", "")
 	textWithSingleSpaceBetween := splitterSpaces.ReplaceAllString(textWithoutTabs, " ")
 
 	textSplitted := strings.Split(textWithSingleSpaceBetween, " ")
-	// textSplitted := splitter.Split(textWithSingleSpaceBetween, -1)
-	fmt.Println("==================")
-	fmt.Println(textSplitted)
-	fmt.Println("==================")
 
 	words := countWordsInSlice(textSplitted)
 
 	sort.Slice(words, func(i, j int) bool { return words[i].Amount > words[j].Amount })
 
-	// fmt.Println("Print inner values")
-	// fmt.Println(len(words))
-	for i, val := range words {
-		fmt.Println(i)
-		fmt.Println(val)
-	}
-	// fmt.Println("End printing inner values")
+	words = dd(words)
 
 	return charItemsToStrings(words)
 }
@@ -46,9 +35,6 @@ func countWordsInSlice(text []string) []ChartItem {
 	words := []ChartItem{}
 
 	for _, word := range text {
-		// fmt.Println(i)
-		// fmt.Println(val)
-
 		// Count how many times current word is presented into text
 		if !contains(words, word) {
 			amount := 0
@@ -70,6 +56,50 @@ func contains(slice []ChartItem, item string) bool {
 		}
 	}
 	return false
+}
+
+func dd(items []ChartItem) []ChartItem {
+	result := [][]ChartItem{}
+	var resultSlice []ChartItem
+
+	fmt.Println(items)
+
+	for i, item := range items {
+
+		switch {
+		case len(resultSlice) == 0:
+			resultSlice = append(resultSlice, item)
+		case i == len(items)-1:
+			resultSlice = append(resultSlice, item)
+			result = append(result, resultSlice)
+		case resultSlice[0].Amount == item.Amount:
+			resultSlice = append(resultSlice, item)
+		case resultSlice[0].Amount > item.Amount:
+			result = append(result, resultSlice)
+			resultSlice = nil
+			resultSlice = append(resultSlice, item)
+		}
+	}
+
+	for _, val := range result {
+		sort.Slice(val, func(i, j int) bool { return val[i].Word < val[j].Word })
+	}
+	fmt.Println(result)
+
+	var top10 []ChartItem
+	j := 0
+	for _, val := range result {
+		for _, val2 := range val {
+			if j == 10 {
+				break
+			}
+			top10 = append(top10, val2)
+			j++
+		}
+	}
+	fmt.Println(top10)
+
+	return top10
 }
 
 func charItemsToStrings(chars []ChartItem) []string {
